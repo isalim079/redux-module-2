61c6f8b2611b5fb13338274b95eed6f7eb48c11f
isalim079 <salimreza1025@gmail.com>
2024-06-29 21:00:32 +0600

    Counter Type added in counter slice

--- src/redux/features/counterSlice.tsx
+++ src/redux/features/counterSlice.tsx
@@ -1,6 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = { count: 0 };
+type CounterType = {
+    count: number;
+};
+
+const initialState: CounterType = { count: 0 };
 
 const counterSlice = createSlice({
     name: "counter",
@@ -12,12 +17,12 @@ const counterSlice = createSlice({
         decrement: (state) => {
             state.count = state.count - 1;
         },
-        incrementByValue: (state, action) => {
+        incrementByValue: (state, action: PayloadAction<number>) => {
             state.count = state.count + action.payload;
         },
     },
 });
 
-export const {increment, decrement, incrementByValue} = counterSlice.actions
+export const { increment, decrement, incrementByValue } = counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
