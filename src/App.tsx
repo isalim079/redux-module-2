816df758c70b2615c62926ded711e580b26546e2
isalim079 <salimreza1025@gmail.com>
2024-06-29 16:19:34 +0600

    Counter card created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
+const App = () => {
+    return (
+        <div className="max-w-2xl mx-auto mt-10">
+            <div className="flex gap-5 items-center">
+                <button className="bg-red-600 text-white px-3 py-2 font-semibold rounded-md shadow-md hover:bg-red-700">Decrement</button>
+                <h1 className="bg-fuchsia-800 text-white px-4 text-xl py-2 font-semibold rounded-md shadow-md">0</h1>
+                <button className="bg-green-600 text-white px-3 py-2 font-semibold rounded-md shadow-md hover:bg-green-700">Increment</button>
+            </div>
+        </div>
+    );
+};
 
-import './App.css'
-
-function App() {
-
-
-  return (
-    <>
-     <h1 className='text-center pt-4 text-xl underline'>React with redux</h1>
-    </>
-  )
-}
-
-export default App
+export default App;
